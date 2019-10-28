cd74ce6674893028363355d41a4d8d3d3e7f224d
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-10-29 00:11:01 +0300

    Move session handlers to session file

---
+++ src/SessionHandlers.hs
@@ -0,0 +1,27 @@
+module SessionHandlers where
+
+import Control.Monad.IO.Class (liftIO)
+import Servant
+import Servant.Auth.Server
+import User
+
+blankSession :: Session
+blankSession = Session "0" "0" 1
+
+createSession ::
+     CookieSettings
+  -> JWTSettings
+  -> AuthData
+  -> Handler (Headers '[ Header "Set-Cookie" SetCookie, Header "Set-Cookie" SetCookie] Session)
+createSession cs jwts (AuthData "lupusanay" "qwerty") = do
+  mApplyCookies <- liftIO $ acceptLogin cs jwts blankSession
+  case mApplyCookies of
+    Nothing -> throwError err401
+    Just cookies -> pure $ cookies blankSession
+createSession _ _ _ = throwError err401
+
+getSession :: AuthResult Session -> Handler Session
+getSession auth = throwError err501
+
+deleteSession :: AuthResult Session -> Handler NoContent
+deleteSession auth = throwError err501
