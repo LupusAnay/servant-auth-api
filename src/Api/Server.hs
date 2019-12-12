02774bcdeb8d8fcdb2b38f94815ead9c2dc701ad
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-12-12 15:55:02 +0300

    Simplify app function

--- src/Api/Server.hs
+++ src/Api/Server.hs
@@ -19,28 +19,29 @@ sessionsServer jwts cs = createSession cs jwts :<|> getSession :<|> deleteSessio
 usersServer :: (MonadDB m, MonadError ServerError m) => ServerT UsersAPI m
 usersServer = createUser :<|> getUser :<|> getUsers :<|> patchUser :<|> deleteUser
 
-checkAuth :: BasicAuthData -> IO (AuthResult AuthSession)
-checkAuth (BasicAuthData "lupusanay" "qwerty") = pure $ Authenticated $ AuthSession "0" "0" 1
+appMToHandler :: Pool -> AppM a -> Handler a
+appMToHandler pool m = runReaderT (runAppM m) pool
+
+hoistServerWithAuth ::
+     HasServer api '[ CookieSettings, JWTSettings]
+  => Proxy api
+  -> (forall x. m x -> n x)
+  -> ServerT api m
+  -> ServerT api n
+hoistServerWithAuth api = hoistServerWithContext api (Proxy :: Proxy '[ CookieSettings, JWTSettings])
 
 app :: Pool -> JWK -> Application
 app pool key = do
   let jwtCfg = defaultJWTSettings key
-      cookieCfg =
-        defaultCookieSettings
-          {cookiePath = Just "/", cookieIsSecure = Servant.Auth.Server.NotSecure, cookieSameSite = AnySite}
-      authCfg = checkAuth
-      cfg = cookieCfg :. jwtCfg :. authCfg :. EmptyContext
-  let server = usersServer :<|> sessionsServer jwtCfg cookieCfg
-  serveWithContext api cfg $
-    hoistServerWithContext api (Proxy :: Proxy '[ CookieSettings, JWTSettings]) appMToHandler server
-  where
-    appMToHandler :: AppM a -> Handler a
-    appMToHandler m = runReaderT (runAppM m) pool
+      cookieCfg = defaultCookieSettings {cookieIsSecure = Servant.Auth.Server.NotSecure, cookieSameSite = AnySite}
+      cfg = cookieCfg :. jwtCfg :. EmptyContext
+      server = usersServer :<|> sessionsServer jwtCfg cookieCfg
+  serveWithContext api cfg $ hoistServerWithAuth api (appMToHandler pool) server
 
 startApp :: IO ()
 startApp = do
   pool <- acquire settings
-  key <- generateKey
+  key <- generateKey -- TODO
   run 8080 $ app pool key
   where
-    settings = (1, 1, "host=localhost port=5432 user=lupusanay dbname=auth password=qwerty")
+    settings = (1, 1, "host=localhost port=5432 user=lupusanay dbname=auth password=qwerty") -- TODO
