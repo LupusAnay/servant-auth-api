11a8f53446656c1a568a8be1b6b62f201cd949e2
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-12-12 20:29:24 +0300

    Implement dummy error handling system

--- src/Api/App.hs
+++ src/Api/App.hs
@@ -2,13 +2,14 @@
 
 module Api.App where
 
-import           Control.Monad.Except   (MonadError)
-import           Control.Monad.IO.Class (MonadIO, liftIO)
-import           Control.Monad.Reader   (ReaderT, ask)
-import           GHC.Generics           (Generic)
-import           Hasql.Pool             (Pool, UsageError, use)
-import           Hasql.Session          (Session)
-import           Servant                (Handler, ServerError)
+import Control.Monad.Except (MonadError, throwError)
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (ReaderT, ask)
+import GHC.Generics (Generic)
+import Hasql.Pool (Pool, UsageError, use)
+import Hasql.Session (Session)
+import Servant (Handler, ServerError)
+import Servant.Server (err500)
 
 class MonadIO m =>
       MonadDB m
@@ -27,3 +28,17 @@ instance MonadDB AppM where
       pool <- ask
       result <- liftIO $ use pool sess
       runAppM $ pure result
+
+class ToServerError a where
+  convert :: a -> ServerError
+
+liftEither' :: (ToServerError a, MonadError ServerError m) => Either a b -> m b
+liftEither' (Right x) = pure x
+liftEither' (Left x) = throwError $ convert x
+
+liftMaybe' :: (MonadError ServerError m) => Maybe a -> m a
+liftMaybe' (Just x) = pure x
+liftMaybe' Nothing = throwError err500
+
+instance ToServerError UsageError where
+  convert err = err500
