a16629f09fe149aca41db50edc263a4429598fe7
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-12-12 20:31:18 +0300

    Implement patchUser and deleteUser methods

--- src/Handlers/User.hs
+++ src/Handlers/User.hs
@@ -3,7 +3,7 @@ module Handlers.User where
 import Api.App
 import Control.Lens
 import Control.Lens.Operators
-import Control.Monad.Except (MonadError, liftEither)
+import Control.Monad.Except (MonadError)
 import Data.AuthSession (AuthSession)
 import qualified Data.ByteString.Char8 as BS
 import qualified Data.ByteString.Lazy as LBS
@@ -17,34 +17,32 @@ import Control.Monad.IO.Class (liftIO, MonadIO)
 
 hashPassword :: (MonadIO m, MonadError ServerError m) => String -> m String
 hashPassword pass = do
-  hashMaybe <- liftIO $ hashPasswordUsingPolicy slowerBcryptHashingPolicy (BS.pack pass)
-  case hashMaybe of 
-    Just hash -> pure $ BS.unpack hash
-    Nothing -> throwError err500
+  hash <- liftMaybe' =<< (liftIO $ hashPasswordUsingPolicy slowerBcryptHashingPolicy $ BS.pack pass)
+  pure $ BS.unpack hash
 
 createUser :: (MonadDB m, MonadError ServerError m) => NewUser -> m UserId
 createUser user = do
   hashedUser <- user & field @"password" hashPassword
-  result <- runSession (Sessions.createUser hashedUser)
-  case result of
-    Right userId -> pure userId
-    Left error -> throwError err500 {errBody = "Something went wrong with database query"}
+  userId <- liftEither' =<< runSession (Sessions.createUser hashedUser)
+  pure userId
 
 getUsers :: (MonadDB m, MonadError ServerError m) => AuthResult AuthSession -> m [User]
 getUsers (Authenticated session) = do
-  result <- runSession Sessions.getAllUsers
-  case result of
-    Right users -> pure users
-    Left error -> throwError err500 {errBody = "Something went wrong with database query"}
+  users <- liftEither' =<< runSession Sessions.getAllUsers
+  pure users
 getUsers _ = throwError err403
 
 getUser :: (MonadDB m) => UserId -> m User
 getUser id = pure $ User 1 "lupusanay" "lupusanay@gmail.com" "qwerty"
 
-patchUser :: (MonadDB m, MonadError ServerError m) => AuthResult AuthSession -> UserId -> NewUser -> m User
-patchUser (Authenticated session) user id = throwError err501
+patchUser :: (MonadDB m, MonadError ServerError m) => AuthResult AuthSession -> UserId -> NewUser -> m NoContent
+patchUser (Authenticated session) id user = do
+ user <- liftEither' =<< runSession (Sessions.updateUser id user)
+ pure NoContent
 patchUser _ _ _ = throwError err403
 
 deleteUser :: (MonadDB m, MonadError ServerError m) => AuthResult AuthSession -> UserId -> m NoContent
-deleteUser (Authenticated session) id = throwError err501
+deleteUser (Authenticated session) id = do
+  liftEither' =<< runSession (Sessions.deleteUser id)
+  pure NoContent
 deleteUser _ _ = throwError err403
