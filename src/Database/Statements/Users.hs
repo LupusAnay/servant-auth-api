b30c83fe9b413835c33f0fd8cfd8e428df49e246
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-12-12 20:30:40 +0300

    Add deleteUser and updateUser statements

--- src/Database/Statements/Users.hs
+++ src/Database/Statements/Users.hs
@@ -1,6 +1,7 @@
 module Database.Statements.Users where
 
-import Data.Profunctor (dimap, rmap)
+import Data.Int (Int32)
+import Data.Profunctor (dimap, lmap, rmap)
 import qualified Data.Text as T
 import Data.User
 import qualified Data.Vector as V
@@ -13,15 +14,42 @@ createUser =
     userEncoder
     userIdDecoder
     [TH.singletonStatement| insert into "users" (username, email, password) values ($1 :: text, $2 :: text, $3 :: text) returning "user_id" :: int4 |]
-  where
-    userEncoder (NewUser nm em pw) = (T.pack nm, T.pack em, T.pack pw)
-    userIdDecoder id = fromIntegral id
 
 getAllUsers :: Statement () [User]
 getAllUsers =
   rmap
     usersDecoder
     [TH.vectorStatement| select "user_id" :: int4, "username" :: text, email :: text, password :: text from "users"|]
+
+deleteUser :: Statement UserId ()
+deleteUser = lmap userIdEncoder [TH.resultlessStatement| delete from "users" where "user_id" = $1 :: int4|]
+
+updateUser :: Statement (UserId, NewUser) ()
+updateUser =
+  lmap
+    userWithIdEncoder
+    [TH.singletonStatement| update "users" set "username" = $2 :: text, email = $3 :: text, password = $4 :: text where "user_id" = $1 :: int4 |]
+
+type NewUserTuple = (T.Text, T.Text, T.Text)
+
+type UserTuple = (Int32, T.Text, T.Text, T.Text)
+
+userWithIdEncoder :: (UserId, NewUser) -> UserTuple
+userWithIdEncoder (id, user) = (userIdEncoder id, name, email, pass)
   where
-    tupleToUser (id, name, email, pass) = User (fromIntegral id) (T.unpack name) (T.unpack email) (T.unpack pass)
-    usersDecoder vec = V.toList $ V.map tupleToUser vec
+    (name, email, pass) = userEncoder user
+
+usersDecoder :: V.Vector UserTuple -> [User]
+usersDecoder vec = V.toList $ V.map userDecoder vec
+
+userDecoder :: UserTuple -> User
+userDecoder (id, name, email, pass) = User (fromIntegral id) (T.unpack name) (T.unpack email) (T.unpack pass)
+
+userIdDecoder :: Int32 -> UserId
+userIdDecoder id = fromIntegral id
+
+userIdEncoder :: UserId -> Int32
+userIdEncoder id = fromIntegral id
+
+userEncoder :: NewUser -> NewUserTuple
+userEncoder (NewUser nm em pw) = (T.pack nm, T.pack em, T.pack pw)
