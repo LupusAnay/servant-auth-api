4d8b42521825e4dd16f4579cb41530927c02972f
Ilya Kopeshtianski <lupusanay@gmail.com>
2019-12-13 18:29:33 +0300

    Change type of time fields in auth to int64

--- src/Data/AuthSession.hs
+++ src/Data/AuthSession.hs
@@ -1,14 +1,15 @@
 module Data.AuthSession where
 
 import           Data.Aeson          (FromJSON, ToJSON)
+import           Data.Int            (Int64)
 import           Data.User           (UserId)
 import           GHC.Generics
 import           Servant.Auth.Server
 
 data AuthSession =
   AuthSession
-    { created :: String
-    , expires :: String
+    { created :: Int64
+    , expires :: Int64
     , userId  :: UserId
     }
   deriving (Show, Generic, FromJSON, ToJSON, ToJWT, FromJWT)
